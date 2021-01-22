77b3f03948ce5066ef45d3f0018bef532c22d942
Kieczkowski, Mateusz <mateusz.kieczkowski@accenture.com>
2021-01-22 23:37:03 +0100

    Main class is no longer Controller. Added DataSource bean and component scan paths

--- DormBookerApi/src/main/java/com/dormbooker/api/DormBookerApiApplication.java
+++ DormBookerApi/src/main/java/com/dormbooker/api/DormBookerApiApplication.java
@@ -1,18 +1,31 @@
 package com.dormbooker.api;
 
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.sql.DataSource;
 import java.util.List;
 
-
-@RestController
 @SpringBootApplication
+@ComponentScan({
+        "com.dormbooker.api.data.controllers",
+        "com.dormbooker.api.data.repositories",
+        "com.dormbooker.api.security.jwt",
+        "com.dormbooker.api.security",
+        "com.dormbooker.api"})
 public class DormBookerApiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
@@ -20,14 +33,19 @@ public class DormBookerApiApplication implements WebMvcConfigurer {
         SpringApplication.run(DormBookerApiApplication.class, args);
     }
 
+    @Value("${spring.datasource.url:url}")
+    private String jdbcURL = "url";
+
+    @Bean(value = "datasource")
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(jdbcURL)
+                .build();
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SpecificationArgumentResolver());
     }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
-    }
-
 }
